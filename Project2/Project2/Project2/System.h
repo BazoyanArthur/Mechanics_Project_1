346be92196afbd8645337e8d812a9813d74f0964
ArthurBazoyan <ArthurBazoyan@github.com>
2020-04-14 22:34:52 +0400

    Final Changes
    Fixed some errors with the directions and the forces

--- Project2/Project2/Project2/System.h
+++ Project2/Project2/Project2/System.h
@@ -1,14 +1,23 @@
 #pragma once
 #include "Mass.h"
+#include "cstdlib"
 
 class System
 {
+	enum DIRECTION
+	{
+		unknown = -2,
+		left = -1,
+		none = 0,
+		right = 1
+	};
+
 public:
 
 	System(Mass m1, Mass m2, Mass m3, double my1, double my2, double my3, double f ) :
 	M1(m1), M2(m2), M3(m3),
 	myu1(my1), myu2(my2), myu3(my3),
-	force(f),
+	force(f), direction(unknown),
 	g(10)
 	{
 		if (!((myu1 > 0 && myu1 <= 0.5) && (myu2 > 0 && myu2 <= 0.5) && (myu3 > 0 && myu3 <= 0.5)))
@@ -18,47 +27,65 @@ public:
 			throw("Force is out of bounds! - at System::System()");
 
 		if (force > 0)
-			direction = true;
-		else
-			direction = false;
+			direction = right;
+		else if (force == 0)
+			direction = none;
+		else if (force < 0)
+			direction = left;
 	}
 
 	double get_m1_accel()
 	{
-		double accel_m1 = (force - ((M1.get_mass() * g) + (M2.get_mass() * g)) * myu1 - (M2.get_mass() * g * myu2)) / M1.get_mass();
-		if (accel_m1 < 0)
+		// if no force is pushin M1 then it has no acceleration
+		if (direction == 0)
 			return 0;
+
+		// if the direction is to the right then 
+		double M1_friction_ground = ((M1.get_mass() * g) + (M2.get_mass() * g)) * myu1;
+		double M1_friction_M2 = (M2.get_mass() * g * myu2);
+		double abs_force = force;
+		//getting the absolute value of force
+		if (force < 0)
+			abs_force = (-1 * force);
+		// if the force isn't enough to move M1 due to its friction
+		if (abs_force < M1_friction_ground + M1_friction_M2)
+			return 0;
+
+		
+		double accel_m1 = (abs_force - M1_friction_ground - M1_friction_M2) / M1.get_mass();
+		
+		if(direction == left)
+			return -accel_m1;
+		
 		return accel_m1;
 	}
 
 	double get_m23_accel()
 	{
-		double f_friction = -(M2.get_mass() * g * myu2);
+		double f_friction = M2.get_mass() * g * myu2;
 
+		// the force pushing M2 in the direction of F applied
 		double f_pushing_m2 = f_friction;
 
-		if (force == 0)
+		if (direction == 0)
 			f_pushing_m2 = 0;
-
-		//if (direction == true)
-		//{
-		//	f_friction = -f_friction;
-		//}
+		
+		//if the direction of movement is to the right then f_pushing is positive else it is negative
+		if (direction == left)
+			f_pushing_m2 = (-1 * f_pushing_m2);
 		
 		double N3 = M1.get_accel() * M3.get_mass();
 		 
 		double accel_m2 = ((M3.get_mass() * g) - (N3 * myu3) - f_pushing_m2 + f_friction) / (M2.get_mass() + M3.get_mass());
 
-		if (force == 0 && accel_m2 < 0)
+		if (accel_m2 <= 0)
 			return 0;
-
 
 		return accel_m2;
 	}
 
 	void PUSH( )
 	{
-
 		// case where f is const
 		M1.set_accel(get_m1_accel());
 		M2.set_accel(get_m23_accel());
@@ -69,6 +96,8 @@ public:
 
 		while (t < 10)
 		{
+			// changing the velocity and finding the distance passed during delta_t
+
 			double dx_m1 = ((M1.get_vel() * delta_t) + M1.get_accel() * ((delta_t * delta_t) / 2));
 			double dv_m1 = M1.get_accel() * delta_t;
 			M1.set_vel(M1.get_vel() + dv_m1);
@@ -82,31 +111,32 @@ public:
 			double dv_m3 = dv_m2;
 			M3.set_vel(M3.get_vel() + dv_m3);
 
-			M1.set_x_cor(M1.get_x_cor() + dx_m1);
-			M3.set_x_cor(M1.get_x_cor());
+			// applying the changes
 
-			M2.set_x_cor(M2.get_x_cor() + dx_m2);
-			if (M2.get_x_cor() < M1.get_x_cor() - 10)
-				M2.set_x_cor(M1.get_x_cor() - 10);	// canot fall
-			else if (M2.get_x_cor() > M1.get_x_cor())
-				M2.set_x_cor(M1.get_x_cor());	// cannot go through pulley
+			M1.set_x_cor(M1.get_x_cor() + dx_m1);	//changing x-coordinate of M1
+			M3.set_x_cor(M1.get_x_cor());			//M3 has the same x-coordinate as M1
 
+			// if M2 is moving right it means M3 is falling down
 			if (M2.get_accel() > 0)
 				dy_m3 = -dy_m3;
 
-			M3.set_y_cor(M3.get_y_cor() + dy_m3);
+			// updating M2 and M3 coordinates when they didn't hit the pulley yet
+			if (M2.get_x_cor() + dx_m2 + dx_m1 < M1.get_x_cor() && M3.get_y_cor() + dy_m3 < M1.get_y_cor())
+			{
+				M2.set_x_cor(M2.get_x_cor() + dx_m2 + dx_m1);
+				M3.set_y_cor(M3.get_y_cor() + dy_m3);
+			}
+			else
+			{
+				M2.set_x_cor(M1.get_x_cor());
+			}
 
-			if (M3.get_y_cor() < 3)
-				M3.set_y_cor(3);	// hit M1
-			else if (M3.get_y_cor() > 10)
-				M3.set_y_cor(10);	// cannot go through pulley
-
+			M3.set_x_cor(M1.get_x_cor());
 
 			std::cout << "M1_x: " << M1.get_x_cor() << "\t" << "M1_y: " << M1.get_y_cor() << "\t" << "M2_x: " << M2.get_x_cor() << "\t" << "M2_y: " << M2.get_y_cor() << "\t" << "M3_x: " << M3.get_x_cor() << "\t" << "M3_y: " << M3.get_y_cor() << std::endl;
 
 			t += delta_t;
 		}
-
 	}
 
 
@@ -115,5 +145,5 @@ private:
 	double myu1, myu2, myu3;
 	double force;
 	int g;
-	bool direction; // false = left, true = right
+	DIRECTION direction;
 };
